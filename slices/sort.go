54af8dc30f880c7fa91abfd52f13d0844be021de
Jorge Gonçalves <jorge.goncalves@movvo.com>
2016-08-16 16:55:19 +0100

    feat(slices): Add slices package.

---
+++ slices/sort.go
@@ -0,0 +1,29 @@
+package src
+
+import "sort"
+
+// Pair is a data structure to hold key/value pairs.
+type Pair struct {
+	Key   int
+	Value int
+}
+
+// PairList is a slice of pairs that implements sort.Interface to sort by values.
+type PairList []Pair
+
+func (p PairList) Len() int           { return len(p) }
+func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
+
+func sortByValues(data map[int]int) PairList {
+	p := make(PairList, len(data))
+
+	i := 0
+	for k, v := range data {
+		p[i] = Pair{k, v}
+		i++
+	}
+
+	sort.Sort(p)
+	return p
+}
