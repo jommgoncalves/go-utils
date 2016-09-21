64254be3217d5e1f92515852999dbaba52d12c34
Jorge Gonçalves <jorge.goncalves@movvo.com>
2016-09-21 16:35:55 +0100

    feat(math): Add MaxFloat function.

--- math/math.go
+++ math/math.go
@@ -2,10 +2,22 @@ package math
 
 import "math"
 
-// Round float.
+// Round Round float (e.g. 0.5 = 1, 0.4 = 0).
+// The parameters is a float64.
+// It returns the value rounded
 func Round(f float64) int {
 	if math.Abs(f) < 0.5 {
 		return 0
 	}
 	return int(f + math.Copysign(0.5, f))
+}
+
+// MaxFloat calculates the max value of the float.
+// The parameters are two floats.
+// It returns the max value.
+func MaxFloat(a float64, b float64) float64 {
+	if a > b {
+		return a
+	}
+	return b
 }
